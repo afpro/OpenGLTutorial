b6fb881842fdce149080388c20d5d3da3ac1ad94
liuyanshuang <liuyanshuang@bytedance.com>
2018-08-03 17:16:57 +0800

    with basic opengl code

--- code/app/src/main/java/gltutorial/afpro/net/gltutorial/MainActivity.kt
+++ code/app/src/main/java/gltutorial/afpro/net/gltutorial/MainActivity.kt
@@ -1,12 +1,35 @@
 package gltutorial.afpro.net.gltutorial
 
-import android.support.v7.app.AppCompatActivity
+import android.opengl.GLES20.*
+import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GLSurfaceView.Renderer {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        glPanel.setEGLContextClientVersion(2)
+        glPanel.setEGLConfigChooser(8, 8, 8, 8, 16, 0)
+        glPanel.setRenderer(this)
+        glPanel.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+    }
+
+    override fun onDrawFrame(ignored: GL10?) {
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+    }
+
+    override fun onSurfaceChanged(ignored: GL10?, width: Int, height: Int) {
+        glViewport(0, 0, width, height)
+    }
+
+    override fun onSurfaceCreated(ignored: GL10?, config: EGLConfig?) {
+        glClearColor(0.3f, 0.6f, 0.9f, 1f)
+        glClearDepthf(1f)
     }
 }
